08da4f9972b26229fe4e34b75b01dbcf3bdf8cfa
DI-GIUSTO-Alex1987 <digiusto.alex@tutanota.com>
2023-12-04 09:40:11 +0100

    version 1.1 premier commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A PROPOS</h1>
+            <br />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid porro provident pariatur doloribus esse, eos qui illo eum! Delectus, voluptas cupiditate labore, quas repudiandae in error impedit ipsum, quod aspernatur eligendi perferendis asperiores iste ducimus eveniet? Quod nisi eligendi optio obcaecati ea fuga cumque sed exercitationem, repudiandae illum, architecto, atque voluptatum. Delectus architecto esse ut quam minus a numquam voluptate dolorem, nam dolorum sapiente laborum, quaerat tempore rerum? Enim reprehenderit fugiat libero accusantium nostrum laudantium illo veritatis, corrupti hic quidem aspernatur provident inventore unde fugit, aut blanditiis similique deserunt, dolorem voluptatibus doloribus! Quia atque facilis nihil aut enim! Doloremque, rerum.</p>
+            
+        </div>
+    );
+};
+
+export default About;
